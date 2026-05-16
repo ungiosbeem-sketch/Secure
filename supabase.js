1d22f0ecda5b17e628c077f14a891fa6affbf6de
ungiosbeem-sketch <ungiosbeem@gmail.com>
2026-05-16 09:13:59 +0300

    Create supabase.js

---
+++ supabase.js
@@ -0,0 +1,15 @@
+import 'react-native-url-polyfill/auto';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import { createClient } from '@supabase/supabase-js';
+
+const supabaseUrl = 'https://ghtpmdxehfuswwxjemyt.supabase.co';
+const supabaseAnonKey = 'sb_publishable_YXOSNcMhtmNdkXwzZYS0oA_eKLcN6HL';
+
+export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
+  auth: {
+    storage: AsyncStorage,
+    autoRefreshToken: true,
+    persistSession: true,
+    detectSessionInUrl: false,
+  },
+});
